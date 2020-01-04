e324e894fa34413889ad57af2241dcb485d73144
DropCodes <sagardb50@gmail.com>
2020-01-04 05:48:29 +0530

    added click listner to github link

---
+++ app.js
@@ -0,0 +1,10 @@
+window.addEventListener('load',()=>{
+    let githubText = document.querySelector('.github-name');
+    let gmailText = document.querySelector('.gmail-name');
+
+    githubText.addEventListener('click',()=>{
+        window.open("https://www.github.com/imdropcodes");
+    })
+
+
+});
