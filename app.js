1b0d1f11b89070ff2e76850a3d4a9ddadefb224d
DropCodes <sagardb50@gmail.com>
2020-01-05 05:11:09 +0530

    added resume

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 window.addEventListener('load',()=>{
     let githubText = document.querySelector('.github-name');
     let gmailText = document.querySelector('.gmail-name');
+    let resumeBtn = document.querySelector('.resume-buton');
 
     githubText.addEventListener('click',()=>{
         window.open("https://www.github.com/imdropcodes");
-    })
+    });
+
+    resumeBtn.addEventListener('click',()=>{
+        
+    });
 
 
 });
