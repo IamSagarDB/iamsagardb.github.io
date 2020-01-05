17191b6f48d27e726a2b0348954ff92749e17989
DropCodes <sagardb50@gmail.com>
2020-01-05 05:32:50 +0530

    Added footer

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 window.addEventListener('load',()=>{
     let githubText = document.querySelector('.github-name');
     let gmailText = document.querySelector('.gmail-name');
-    let resumeBtn = document.querySelector('.resume-buton');
 
     githubText.addEventListener('click',()=>{
         window.open("https://www.github.com/imdropcodes");
     });
 
-    resumeBtn.addEventListener('click',()=>{
-        
-    });
+
 
 
 });
